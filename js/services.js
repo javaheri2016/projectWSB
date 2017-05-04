5253251d5c26c61a15f4058bb8e5c214a217bee8
javaheri2016 <katarzyna.javaheri@gmail.com>
2017-05-04 20:57:51 +0200

    4 maja slajder nowe obrazy

--- js/services.js
+++ js/services.js
@@ -7,176 +7,92 @@
     	let itemPear = [
     		{	
     			id: '1',
-    			name: 'PearPhone',
-    			price: '4525zł',
-    			description: 'Najlepszy model, najnowszy model',
-    			photo: './grafika/item1.jpg',
+    			name: 'PearWatch Series 2',
+    			price: '1525zł',
+    			description: 'Najnoszy model w przystępnej cenie',
+    			photo: './grafika/items/pearwatch1.png',
+                link: 'product1.html'
     				
     		
     		},
     		{
     			id: '2',
-    			name: 'Pear',
-    			price: '1214zł',
-    			description: 'Lorem ipsum dolor sit amet, consectetur',
-    			photo: './grafika/item2.jpg'
+    			name: 'PearWatch+',
+    			price: '1715zł',
+    			description: 'Wyższy model+ w najlepszej cenie',
+    			photo: './grafika/items/pearwatch2.png',
+                link: 'product2.html'
     		},
     		{ 
     			id: '3',
-    			name: 'PearPC Air',
-    			price: '7585zł',
-    			description: 'Lorem ipsum dolor si',
-    			photo: './grafika/item3.jpg'
+    			name: 'PearWatch ProEdition',
+    			price: '2585zł',
+    			description: 'Model, od którego możesz oczekiwać wszystkiego.',
+    			photo: './grafika/items/pearwatch3.png'
     		},
     		{
     			id: '4',
-    			name: 'iPear Mini',
-    			price: '999zł',
-    			description: 'Lorem ipsum dolor sit ame',
-    			photo: './grafika/item3.jpg'
+    			name: 'PearPC',
+    			price: '2999zł',
+    			description: 'Niezawodny laptop na lata',
+    			photo: './grafika/items/pearpc1.png'
     		},
     		{	
     			id: '5',
-    			name: 'PearPC',
+    			name: 'PearPC Air',
     			price: '4525zł',
-    			description: 'Najlepszy model, najnowszy model',
-    			photo: './grafika/item3.jpg'
+    			description: 'Lekki, nowoczesny laptop z możliwościami komputera biurkowego',
+    			photo: './grafika/items/pearpc2.png'
     		},
     		{
     			id: '6',
-    			name: 'Pear',
-    			price: '1214zł',
-    			description: 'Lorem ipsum dolor sit amet, consectetur',
-    			photo: './grafika/item1.jpg'
+    			name: 'PearPC Pro',
+    			price: '12300zł',
+    			description: 'Komputer biurkowy, od którego możesz wymagać najbardziej zaawansowanych rzeczy.',
+    			photo: './grafika/items/pearpc3.png'
     		},
     		{ 
     			id: '7',
-    			name: 'PearPC Air',
-    			price: '7585zł',
-    			description: 'Lorem ipsum dolor si',
-    			photo: './grafika/item3.jpg'
+    			name: 'iPear Pro',
+    			price: '3585zł',
+    			description: 'Najlepszy model w swojej klasie',
+    			photo: './grafika/items/ipear1.png'
     		},
     		{
     			id: '8',
     			name: 'iPear Mini',
-    			price: '999zł',
-    			description: 'Lorem ipsum dolor sit ame',
-    			photo: './grafika/item2.jpg'
+    			price: '1299zł',
+    			description: 'Mały wymiar, ogromne możliwości',
+    			photo: './grafika/items/ipear3.png'
     		},
     		{
     			id: '9',
-    			name: 'Pear',
-    			price: '1214zł',
-    			description: 'Lorem ipsum dolor sit amet, consectetur',
-    			photo: './grafika/item1.jpg'
+    			name: 'iPear Air 2',
+    			price: '2214zł',
+    			description: 'Lekki, wytrzymały, najlepszy',
+    			photo: './grafika/items/ipear2.png'
     		},
     		{ 
     			id: '10',
-    			name: 'PearPC Air',
-    			price: '7585zł',
-    			description: 'Lorem ipsum dolor si',
-    			photo: './grafika/item1.jpg'
+    			name: 'PearPhone 7',
+    			price: '2585zł',
+    			description: 'Najnowszy model na rynku',
+    			photo: './grafika/items/pearphone1.png'
     		},
     		{
     			id: '11',
-    			name: 'iPear Mini',
-    			price: '999zł',
-    			description: 'Lorem ipsum dolor sit ame',
-    			photo: './grafika/item3.jpg'
+    			name: 'PearPhone 6',
+    			price: '1599zł',
+    			description: 'Optymalny wybór dla wymagających',
+    			photo: './grafika/items/pearphone2.png'
     		},
             {
                 id: '12',
-                name: 'iPear Mini',
+                name: 'PearPhone SE',
                 price: '999zł',
-                description: 'Lorem ipsum dolor sit ame',
-                photo: './grafika/item3.jpg'
+                description: 'Wysoka jakość w dobrej cenie',
+                photo: './grafika/items/pearphone3.png'
             },
-                    {   
-                id: '13',
-                name: 'PearPhone',
-                price: '4525zł',
-                description: 'Najlepszy model, najnowszy model',
-                photo: './grafika/item1.jpg',
-                    
-            
-            },
-            {
-                id: '14',
-                name: 'Pear',
-                price: '1214zł',
-                description: 'Lorem ipsum dolor sit amet, consectetur',
-                photo: './grafika/item2.jpg'
-            },
-            { 
-                id: '15',
-                name: 'PearPC Air',
-                price: '7585zł',
-                description: 'Lorem ipsum dolor si',
-                photo: './grafika/item3.jpg'
-            },
-            {
-                id: '16',
-                name: 'iPear Mini',
-                price: '999zł',
-                description: 'Lorem ipsum dolor sit ame',
-                photo: './grafika/item3.jpg'
-            },
-            {   
-                id: '17',
-                name: 'PearPC',
-                price: '4525zł',
-                description: 'Najlepszy model, najnowszy model',
-                photo: './grafika/item3.jpg'
-            },
-            {
-                id: '18',
-                name: 'Pear',
-                price: '1214zł',
-                description: 'Lorem ipsum dolor sit amet, consectetur',
-                photo: './grafika/item1.jpg'
-            },
-            { 
-                id: '19',
-                name: 'PearPC Air',
-                price: '7585zł',
-                description: 'Lorem ipsum dolor si',
-                photo: './grafika/item3.jpg'
-            },
-            {
-                id: '20',
-                name: 'iPear Mini',
-                price: '999zł',
-                description: 'Lorem ipsum dolor sit ame',
-                photo: './grafika/item2.jpg'
-            },
-            {
-                id: '21',
-                name: 'Pear',
-                price: '1214zł',
-                description: 'Lorem ipsum dolor sit amet, consectetur',
-                photo: './grafika/item1.jpg'
-            },
-            { 
-                id: '22',
-                name: 'PearPC Air',
-                price: '7585zł',
-                description: 'Lorem ipsum dolor si',
-                photo: './grafika/item1.jpg'
-            },
-            {
-                id: '23',
-                name: 'iPear Mini',
-                price: '999zł',
-                description: 'Lorem ipsum dolor sit ame',
-                photo: './grafika/item3.jpg'
-            },
-            {
-                id: '24',
-                name: 'iPear Mini',
-                price: '999zł',
-                description: 'Lorem ipsum dolor sit ame',
-                photo: './grafika/item3.jpg'
-            }
     	];
 	
 	
